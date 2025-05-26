2ef87eac9251c4c585d2e535fedd9da8eb46fa34
Freddie Beasley <frederickbeasley1210+github@gmail.com>
2025-05-26 12:46:10 +0100

    🖋️ Created Vector3 class which inherits from Vector2

--- bouncingParticle.py
+++ bouncingParticle.py
@@ -49,3 +49,53 @@ class Vector2:
           self.updateX(dXvalue)
           self.updateY(dYvalue)
 
+class Vector3(Vector2):
+     def __init__(self, Xcomponent, Ycomponent, Zcomponent):
+          super().__init__(Xcomponent, Ycomponent)
+
+          self.__Zcomponent = Zcomponent
+
+     #getter - inherits getX and getY
+
+     def getZ(self):
+          return self.__Zcomponent
+     
+     def get_cartesian(self):
+          return [self.getX(), self.getY(), self.getZ()]
+     
+     def get_magnitude(self):
+          xSquared = (self.getX())**2
+          ySquared = (self.getY())**2
+          zSquared = (self.getZ())**2
+          return (xSquared + ySquared + zSquared)**(1/2)
+     
+     def get_theta(self):
+          theta = math.atan2(self.getY(), self.get_X())
+
+     def get_phi(self):
+          phi = math.acos(self.getZ(), self.get_magnitude())
+     
+     def get_direction(self):
+          return [self.get_theta(), self.get_phi()]
+     
+     def get_polar(self):
+          return [self.get_magnitude(), self.get_theta(), self.get_phi()]
+     
+     #setter - inherits setX and setY
+     def setZ(self, value):
+          self.__Zcomponent = value
+     
+     def set(self, Xvalue, Yvalue, Zvalue):
+          self.setX(Xvalue)
+          self.setY(Yvalue)
+          self.getZ(Zvalue)
+     
+     #updater - inherits updateX and updateY
+     def updateZ(self, dZvalue):
+          self.__Zcomponent += dZvalue
+     
+     def update(self, dXvalue, dYvalue, dZvalue):
+          self.updateX(dXvalue)
+          self.updateY(dYvalue)
+          self.updateZ(dZvalue)
+     
